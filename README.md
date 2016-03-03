a5687987f764b1b054b20be595a6bdc1dbf52b9f
Giso Stallenberg <gisostallenberg@gmail.com>
2016-03-03 17:17:29 +0100

    Initial fileserver * Fileserver class * README * composer requirements

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # file-serving
 Serve files to the browser that are not available directly
+
+## Installation
+```bash
+composer require gisostallenberg/file-serving
+```
+
+## Usage example
+```php
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use GisoStallenberg\FileServing\FileServer;
+
+$fileserver = new FileServer('../serve-me/', 'serve-it/');
+$fileserver->serve(); // will server http://example.com/serve-it/example.txt when ../serve-me/example.txt exists, gives a 404 otherwise
+```
+
+```php
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use GisoStallenberg\FileServing\FileServer;
+use Symfony\Component\HttpFoundation\Response;
+
+$fileserver = new FileServer('../serve-me/', 'serve-it/');
+$response = $fileserver->getResponse(); // do not serve yet
+
+if ($response->getStatusCode() === Response::HTTP_NOT_FOUND) {
+    $fileserver = new FileServer('../serve-other-dir/', 'serve-it/'); // check another directory
+    $response = $fileserver->getResponse();
+}
+
+$response->send();
+```
+
+## Credits
+Niels Nijens (https://github.com/niels-nijens/)
--- src/FileServer.php
+++ src/FileServer.php
@@ -2,10 +2,109 @@
 
 namespace GisoStallenberg\FileServing;
 
+use DateTime;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FileServer
 {
-    
+    /**
+     * The path to serve from.
+     *
+     * @var string
+     */
+    private $from;
+
+    /**
+     * The server path to serve.
+     *
+     * @var string
+     */
+    private $to;
+
+    /**
+     * The request object.
+     *
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * The base path to find files.
+     *
+     * @var string
+     */
+    private $basePath;
+
+    /**
+     * Constructor.
+     *
+     * @param string  $from
+     * @param string  $to
+     * @param Request $request
+     */
+    public function __construct($from, $to, Request $request = null)
+    {
+        $this->from = $from;
+        $this->to = $to;
+
+        if (is_null($request)) {
+            $request = Request::createFromGlobals();
+        }
+        $this->request = $request;
+
+        $this->basePath = $this->request->server->get('DOCUMENT_ROOT');
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param string  $from
+     * @param string  $to
+     * @param Request $request
+     *
+     * @return \static
+     */
+    public static function create($from, $to, Request $request = null)
+    {
+        return new static($from, $to, $request);
+    }
+
+    /**
+     * Creates the response object.
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        $requestPath = parse_url($this->request->server->get('REQUEST_URI'), PHP_URL_PATH);
+
+        $filePath = $this->basePath.$this->from.substr($requestPath, strlen($this->to));
+
+        if (is_file($filePath)) {
+            $file = new File($filePath);
+            $response = Response::create()
+                ->setExpires(new DateTime('+1 week'))
+                ->setLastModified(DateTime::createFromFormat('U', $file->getMTime()));;
+
+            if ($response->isNotModified($this->request)) {
+
+                return $response;
+            }
+
+            $response->headers->set('Content-Type', $file->getMimeType() ?: 'application/octet-stream');
+            return $response->setContent(file_get_contents($file->getPathname()));
+        }
+
+        return Response::create('', Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * Serves the file.
+     */
+    public function serve()
+    {
+        $this->getResponse()->send();
+    }
 }
